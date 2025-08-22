5e5b64d40d9bc84ce3fa469477b048a3342ba88e
SUNNY KUSHAWAHA <96289000+SUNNYKUSHWAHA07@users.noreply.github.com>
2025-08-22 06:26:19 +0530

    changed the cors

--- Backend/index.js
+++ Backend/index.js
@@ -11,12 +11,7 @@ const _dirname = path.resolve();
 const app = express();
 connectDB();
 
-app.use(cors({
-  origin: "https://crud-task-15do.onrender.com",
-  credentials: true,
-}
-  
-));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
